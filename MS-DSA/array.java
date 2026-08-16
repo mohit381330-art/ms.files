7a289e5a6a9ae5277d4bd5e26500a3de3893ece7
@mvs-786 <mohit381330@gmail.com>
2026-08-16 11:23:59 +0530

    Update array.java

--- MS-DSA/array.java
+++ MS-DSA/array.java
@@ -72,6 +72,20 @@ public class Main {
 }
 
 
+// Single Number
+public class Main {
+    public static void main(String[] args) {
+        int ans = 0;
+        int[] arr = {2, 2, 2, 4}; // जावा में एरे का सिंटैक्स
+        
+        for (int i : arr) {
+            ans ^= i; // XOR ऑपरेशन
+        }
+        
+        System.out.println(ans);
+    }
+}
+
 //  MSS (Kadane's Algorithm)
 class Solution {
     public int maxSubArray(int[] nums) {
