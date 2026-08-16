72c19c017c04d91b105704c91f8a4a9549ed9194
@mvs-786 <mohit381330@gmail.com>
2026-08-16 11:04:15 +0530

    Refactor vector.java to keep only single number logic
    Removed unused vector examples and retained single number calculation.

--- MS-DSA/vector.java
+++ MS-DSA/vector.java
@@ -1,32 +1,3 @@
-// Vector Syntax
-import java.util.ArrayList;
-import java.util.Arrays;
-public class Main {
-    public static void main(String[] args) {
-        // C++: vector<int> vec = {1, 2, 3};
-        ArrayList<Integer> vec = new ArrayList<>(Arrays.asList(1, 2, 3));
-        
-        // C++: vec[2]
-        System.out.println(vec.get(2)); 
-    }
-}
-
-
-// Loops in Vector
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public class Main {
-    public static void main(String[] args) {
-        ArrayList<Integer> vec = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
-        
-        // For-Each Loop (बिल्कुल C++ जैसा ही है)
-        for (int i : vec) {
-            System.out.println(i);
-        }
-    }
-}
-
 
 // Single Number
 public class Main {
